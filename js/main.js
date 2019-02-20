4eeb0af37dde0d72cfa809a17240b88d431b2f33
geenkonzo <gcobisakonzo@gmail.com>
2019-02-20 11:38:23 +0200

    created a demo code for hide and show of divs on tab click

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('#hide').click(function() {
+    $('#content1').hide();
+    $('#content2').show();
+});
+
+$('#show').click(function() {
+    $('#content1').show();
+    $('#content2').hide();
+});
