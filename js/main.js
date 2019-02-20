41fb27b48645e464e651a11e6f1cb6509d3f4a37
geenkonzo <gcobisakonzo@gmail.com>
2019-02-20 11:53:23 +0200

    added basic progress bar

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ $('#show').click(function() {
     $('#content1').show();
     $('#content2').hide();
 });
+
+function move() {
+    var elem = document.getElementById("bar");   
+    var width = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 26) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%';
+      }
+    }
+} 
